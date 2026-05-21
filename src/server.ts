0fdeac36dcd8769edcf5d4113a70010b781f6a1f
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-22 05:16:37 +0900

    BRR: initial setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("nma gap xaxaxaxaxa xixixixixixix");
+
+import moment from "moment";
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Sanjar";
+const count: number = 25;
