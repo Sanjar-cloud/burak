3668453b505131ce81b97f02e8437c3602d5f1be
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-25 05:01:01 +0900

    feat: algaritim task - M

--- train.ts
+++ train.ts
@@ -1,15 +1,35 @@
 
-function getReverse(hello: string): string {
+// function getReverse(hello: string): string {
 
-  let words: string[] = hello.split(" ");
-  let boshjoy: string[] = [];
+//   let words: string[] = hello.split(" ");
+//   let boshjoy: string[] = [];
 
-  for (let word of words) {
-    let rev: string = word.split("").reverse().join("");
-    boshjoy.push(rev);
+//   for (let word of words) {
+//     let rev: string = word.split("").reverse().join("");
+//     boshjoy.push(rev);
+//   }
+
+//   return boshjoy.join(" ");
+// }
+
+// console.log(getReverse("we like codding")); // i ekil gnidoc
+
+
+//m-task
+
+function getSquareNumbers(sonlar: number[]): object[] {
+
+  let result: object[] = []
+
+  for (let numb of sonlar) {
+
+    let obj = { n: numb, s: numb * numb }
+
+    result.push(obj)
   }
 
-  return boshjoy.join(" ");
+  return result
 }
 
-console.log(getReverse("we like codding")); // i ekil gnidoc
+console.log(getSquareNumbers([2, 5, 7]));
+
