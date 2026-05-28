d05aedbe329a0483727443379e7614923cc50e56
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-29 05:01:00 +0900

    feat: member service model integration and develop member schema model

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import MemberService from "../models/Member.service"; 
 
 const restaurantController: T = {};
 
@@ -28,4 +29,4 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 };
 
 export default restaurantController;
- 
+  
