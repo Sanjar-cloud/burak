21b24037a8bb05050cf275d483459c06455de705
sanjar <sanjarbekkarimov229@gmail.com>
2026-06-05 08:14:36 +0900

    feat: frontend related css, js, image creation and dev admin  frontend pages and integrate header & footer

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ mongoose.connect(process.env.MONGO_URL  as string, {}) // mongoose orrqali mongo
     console.log('mongodb connection sucessfully on port')
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function()  { // app va backend portda 
-        console.log(`the server is running sucessfully on part: ${PORT}`);
+        console.info(`the server is running sucessfully on part: ${PORT}`);
+        console.info(`Admin project on http://localhost:${PORT}/admin \n `);
     }); 
 })
-.catch((err) => console.log('ERROR connection on mongodb', err));   
+.catch((err) => console.log('ERROR connection on mongodb', err));    
