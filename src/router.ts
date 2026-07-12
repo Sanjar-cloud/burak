1f7565b2f9ba0e36a5df0b5d7b744c1ec125a50a
sanjar <sanjarbekkarimov229@gmail.com>
2026-07-13 02:52:08 +0900

    feat: develop createOrder rest api

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import makeUploader from "./libs/utils/uploader";
 import productController from "./controllers/product.controller";
+import orderController from "./controllers/order.controller";
 
 /** Member **/
 router.get("/member/restaurant", memberController.getRestaurant)
@@ -38,6 +39,13 @@ router.get(
 
 
 /** Order **/
+router.post(
+  "/order/create",
+  memberController.verifyAuth,
+  orderController.createOrder
+  
+);
+
  
 
 export default router;
